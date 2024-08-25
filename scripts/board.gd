9ac5495c3ab4810b30757a4020fd95123596cd2e
pegasojuegos <mhp74185@gmail.com>
2024-08-25 18:12:35 +0200

    Added medication and crazy patient system
    Pending adding crazy patients atck other patients

--- scripts/board.gd
+++ scripts/board.gd
@@ -4,17 +4,22 @@ extends Node2D
 	{"ingredients": {"Pharmacy": 1, "Patient": 1}, "result": {"Drugs":2}},
 	{"ingredients": {"Drugs": 3, "Patient":1},"result": {"Medication": 2}},
 ]
-@export var turnsPerDay: int = 5
+@export var turnsPerDay: int = 3
 var turnsLeft: int = turnsPerDay
 var days: int = 1
 var patientsInGame: Array = []
 var medicationsInGame: Array = []
 
 func _ready():
-	var patient = preload("res://scenes/deck/patient.tscn").instantiate()
-	patient.position.x = -100
-	patientsInGame.append(patient)
-	$CardsInGame.add_child(patient)
+	$Label.text = "Days: "+str(days)+"\n Turns left: "+str(turnsLeft)
+	
+	#Generate initial patients
+	var patientsNumber = 4
+	for i in range(patientsNumber):
+		var patient = preload("res://scenes/deck/patient.tscn").instantiate()
+		patient.position.x = -100
+		patientsInGame.append(patient)
+		$CardsInGame.add_child(patient)
 	var pharmacy = preload("res://scenes/deck/pharmacy.tscn").instantiate()
 	pharmacy.position.x = 50
 	$CardsInGame.add_child(pharmacy)
@@ -43,17 +48,25 @@ func checkCrafting(selectedCards: Array):
 		
 		# Check if needed ingredients are present
 		for ingredient in ingredientsNeeded.keys():
-			if not ingredientsCount.has(ingredient) or ingredientsCount[ingredient] < ingredientsNeeded[ingredient]:
+			if not ingredientsCount.has(ingredient) or ingredientsCount[ingredient] != ingredientsNeeded[ingredient]:
 				craftable = false
 				break # If one ingredient is missing the craft doesn't match
 		
+		# Check if there is no extra ingredients
+		for ingredient in ingredientsCount.keys():
+			if not ingredientsNeeded.has(ingredient):
+				craftable = false
+				break
+		
 		# If all ingredients are present and can play turn call craft and use consumables
-		if craftable and playTurns(1):
+		if craftable and canPlayTurns(1):
 			createCraft(craft["result"],selectedCards[0].position)
 			
 			for card in selectedCards:
 				if card is Consumable:
 					card.use(1)
+			
+			playTurns(1)
 
 func createCraft(cards: Dictionary, newPositon: Vector2):
 	for card in cards.keys():
@@ -76,17 +89,33 @@ func createCraft(cards: Dictionary, newPositon: Vector2):
 			else:
 				print("Failed to load scente: " + cardPath)
 
-func playTurns(numberOfTurns) -> bool:
-	var canPlayTurns = true
-	# You can't spend more turns than turns left
-	if (numberOfTurns < turnsLeft):		
-		turnsLeft -= numberOfTurns
-		print(turnsLeft," ",days)
-		
-		# Next day logic
-		if turnsLeft <= 0:
-			days += 1
-			turnsLeft = turnsPerDay
-		
-	else: canPlayTurns = false
-	return canPlayTurns
+# You can't spend more turns than turns left
+func canPlayTurns(numberOfTurns):
+	return numberOfTurns <= turnsLeft
+
+func playTurns(numberOfTurns):		
+	turnsLeft -= numberOfTurns
+	
+	# Next day logic
+	if turnsLeft <= 0:
+		nextDay()
+	
+	$Label.text = "Days: "+str(days)+"\n Turns left: "+str(turnsLeft)
+
+func nextDay():
+	days += 1
+	turnsLeft = turnsPerDay
+	
+	if medicationsInGame >= patientsInGame:
+		#Use one medication for each patient
+		for i in range(patientsInGame.size()):
+			medicationsInGame[i].use(1)
+		print("Life")
+	else:
+		#Use the medications aviable, and go crazy the patients wit no medication
+		for i in range(medicationsInGame.size()):
+			medicationsInGame[i].use(1)
+		 
+		for i in range(patientsInGame.size() - medicationsInGame.size()):
+			patientsInGame[i].goCrazy()
+		print("Die")
