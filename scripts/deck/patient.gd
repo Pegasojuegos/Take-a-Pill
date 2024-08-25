027a5becdd32ea9bc379881afe16fc60ac6c49d7
pegasojuegos <mhp74185@gmail.com>
2024-08-25 23:17:18 +0200

    Implemented new crazy sprite

--- scripts/deck/patient.gd
+++ scripts/deck/patient.gd
@@ -9,4 +9,4 @@ func _init():
 	super._init(patientName, patientDescription, patientLige, patientDamage)
 
 func goCrazy():
-	$Sprite2D.texture = load("res://assets/images/deck/card_flesh.png")
+	$Sprite2D.texture = load("res://assets/images/deck/card_crazy_patient.png")
